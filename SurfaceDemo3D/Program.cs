9bbcf8a183adbe76fd99bd34e09001746db26d3c
xybyn <nikishtm@gmail.com>
2021-01-26 16:34:10 +0700

    added surface demo

---
+++ SurfaceDemo3D/Program.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using System;
+
+namespace SurfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = GameWindowSettings.Default;
+            var nativeWindowSettings = new NativeWindowSettings
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Surface"
+            };
+
+            using (var window = new SurfaceDemo3DWindow(settings, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
---
+++ SurfaceDemo3D/SurfaceDemo3DWindow.cs
@@ -0,0 +1,47 @@
+﻿// unset
+
+using Common;
+using GlmNet;
+using OpenTK.Windowing.Desktop;
+using static System.MathF;
+
+namespace SurfaceDemo
+{
+    public class SurfaceDemo3DWindow : Window3D
+    {
+        public SurfaceDemo3DWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+
+            var surface = new Surface((x, z) => x * z / 5, new SurfaceDrawer3DSettings
+            {
+                MaxX = 4,
+                MinX = -4,
+                MaxZ = 4,
+                MinZ = -4,
+                NumberOfPartitions = 120
+            });
+            AddGrid();
+            surface.Material.Color = new vec3(0.6f, 0.6f, 0);
+            ToDraw.Add(surface);
+            surface.IsShowingNormals = true;
+            surface.Scale(new vec3(1, 0.5f, 1));
+            var surface2 = new Surface((u, v) =>
+                new vec3(
+                    1 * Cos(u)*Cos(v),
+                    1 * Sin(u)*Cos(v),
+                    1 * Sin(v)
+                    ), new SurfaceDrawer3DSettings
+            {
+                MaxX = 2*PI,
+                MinX = 0,
+                MaxZ = PI/2,
+                MinZ = -PI/2,
+                NumberOfPartitions = 60
+            }, true);
+            surface2.TranslateGlobal(new vec3(5, 3, 0));
+            surface2.Material.Color = new vec3(0.6f, 0, 0.6f);
+         
+            ToDraw.Add(surface2);
+        }
+    }
+}
